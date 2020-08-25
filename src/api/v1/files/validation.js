bf258880b855167d003009f6187b5eb71e7503a9
Manish Budhiraja <iammanishbudhiraja@gmail.com>
2020-08-25 11:03:30 +0530

    validations issue fixed

--- src/api/v1/files/validation.js
+++ src/api/v1/files/validation.js
@@ -1,16 +1,16 @@
 const { Joi } = require('express-validation');
 
 module.exports = {
-  deleteFile: { params: { _id: Joi.string().required() } },
+  deleteFile: Joi.object({ params: { _id: Joi.string().required() } }),
   download: {
-    params: { _id: Joi.string().required() },
-    query: {
+    params: Joi.object({ _id: Joi.string().required() }),
+    query: Joi.object({
       accessToken: Joi.string().required(),
       format: Joi.string()
         .valid('png', 'jpeg', 'jpg', 'mp4', 'mov')
         .default('png'),
       height: Joi.number().required(),
       width: Joi.number().required(),
-    },
+    }),
   },
 };
