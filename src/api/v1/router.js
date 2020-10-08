9be678a0008b355948f470128108ee052ec5a17b
Manish Budhiraja <22340444+imbudhiraja@users.noreply.github.com>
2020-10-08 19:07:34 +0530

    Updated routes

--- src/api/v1/router.js
+++ src/api/v1/router.js
@@ -5,10 +5,6 @@ const userRoutes = require('./user/routes');
 const router = express.Router();
 
 /**
- * GET v1/status
- */
-router.get('/status', (req, res) => res.send('OK'));
-/**
  * GET v1/docs
  */
 router.use('/docs', express.static('docs'));
